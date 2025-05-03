8841918ff3c9f49af75d62f4123b6f53359a407b
roldleo <haroldleopanja@gmail.com>
2025-05-03 08:49:57 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const server = express();
+
+  server.use(
+    cors({
+      origin: [
+        'http://localhost:3000',
+        'https://self-management-xi.vercel.app',
+      ],
+      credentials: true,
+    }),
+  );
+
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors({
-    origin: ['http://localhost:3000/', 'https://self-management-xi.vercel.app'],
-    credentials: true,
-  });
+
   await app.init();
+
   server.listen(3000, () => console.log('Server running...'));
 }
 bootstrap();
